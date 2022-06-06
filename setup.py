00ed88a3877f25f5defecd8bb107c52461091dfb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-06 21:21:02 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+NAME = 'infections'
+VERSION = '1.0.0'
+DESCRIPTION = 'Masters Project'
+AUTHOR = 'greyhypotheses'
+URL = 'https://github.com/helminthiases/infections'
+PYTHON_REQUIRES = '>=3.7.11'
+
+with open('README.md') as f:
+    readme = f.read()
+
+setuptools.setup() (
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=readme,
+    author=AUTHOR,
+    url=URL,
+    python_requires=PYTHON_REQUIRES,
+    packages=setuptools.find_packages(exclude=['docs', 'tests'])
+)
