cb24632c19bf15074a459bbfd85b1a264ba70060
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-07 23:55:14 +0100

    Update src/data/ReadKey.py

--- src/data/ReadKey.py
+++ src/data/ReadKey.py
@@ -6,12 +6,12 @@ class ReadKey:
 
     def __init__(self):
 
-        self.filestr = os.path.join(os.getcwd(), 'keys', 'api.json')
+        self.source = os.path.join(os.getcwd(), 'keys', 'api.json')
 
     def exc(self):
 
         try:
-            readings = pd.read_json(path_or_buf=self.filestr)
+            readings = pd.read_json(path_or_buf=self.source)
         except OSError as err:
             raise err
 
