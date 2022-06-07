9cee59deac2b0d1a6eb113c7de4c3c52df761171
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-07 23:53:16 +0100

    Add src/main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import logging
+import os
+import sys
+
+
+def main():
+
+    logger.info('infections')
+
+
+if __name__ == '__main__':
+
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    main()
