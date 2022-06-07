3e09c134c9330afeb1cc6e1721855c5f68b6626d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-07 22:07:21 +0100

    Add src/data/FieldExperiment.py

---
+++ src/data/FieldExperiment.py
@@ -0,0 +1,18 @@
+
+
+class FieldExperiment:
+
+    def __init__(self, fields):
+
+        # The API end point
+        self.endpoint = 'https://admin.espen.afro.who.int/'
+
+        # API fields of interest
+        self.fields = fields
+
+    def exc(self):
+        """
+        In progress
+
+        :return:
+        """
