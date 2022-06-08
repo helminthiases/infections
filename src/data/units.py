273c5ab492ec7cc6e560701d5dfb00855bca4936
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-08 23:53:45 +0100

    Update src/data/units.py

--- src/data/units.py
+++ src/data/units.py
@@ -1,3 +1,7 @@
+import src.data.espen
+
+import pandas as pd
+
 
 class Units:
 
@@ -8,11 +12,26 @@ class Units:
         """
 
         self.key = key
-        self.fields = {'ADMIN0': 'admin0', 'ADMIN1': 'admin1', 'ADMIN1D': 'admin1_id', 'ADMIN2': 'admin2_id',
-                       'IUs_ADM': 'iu_adm', 'IUs_NAME': 'iu_name', 'IU_ID': 'iu_id', 'IU_CODE': 'iu_code'}
+        self.fields: dict = {'ADMIN0': 'admin0', 'ADMIN1': 'admin1', 'ADMIN1D': 'admin1_id', 'ADMIN2': 'admin2_id',
+                             'IUs_ADM': 'iu_adm', 'IUs_NAME': 'iu_name', 'IU_ID': 'iu_id', 'IU_CODE': 'iu_code'}
+
+    def __structure(self, data: pd.DataFrame):
+
+        units = data.copy().loc[:, list(self.fields.keys())].drop_duplicates()
+        units.rename(columns=self.fields, inplace=True)
+
+        return units
 
     def exc(self):
         """
 
         :return:
         """
+
+        objects = src.data.espen.ESPEN(base='cartographies').request(
+            params={'api_key': self.key})
+
+        frame = pd.DataFrame.from_records(objects)
+        frame = self.__structure(data=frame)
+
+        return frame
