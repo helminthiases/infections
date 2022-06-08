babd375f6865e136292009c963020298d05fb584
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-08 23:40:27 +0100

    Update src/data/units.py

--- src/data/units.py
+++ src/data/units.py
@@ -8,8 +8,8 @@ class Units:
         """
 
         self.key = key
-        self.fields = ['year', 'continent', 'region', 'who_region', 'admin0', 'admin0_id',
-                       'iso2', 'iso3', 'admin_level']
+        self.fields = {'ADMIN0': 'admin0', 'ADMIN1': 'admin1', 'ADMIN1D': 'admin1_id', 'ADMIN2': 'admin2_id',
+                       'IUs_ADM': 'iu_adm', 'IUs_NAME': 'iu_name', 'IU_ID': 'iu_id', 'IU_CODE': 'iu_code'}
 
     def exc(self):
         """
