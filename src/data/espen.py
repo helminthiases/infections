08515af443f6c1ad74c81db12f89eeae0c06b280
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-08 19:11:26 +0100

    Update src/data/espen.py

--- src/data/espen.py
+++ src/data/espen.py
@@ -3,35 +3,20 @@ import requests
 
 class ESPEN:
 
-    def __init__(self, fields):
+    def __init__(self, base: str):
 
         # The API end point
-        self.endpoint = 'https://admin.espen.afro.who.int/'
+        self.endpoint = 'https://admin.espen.afro.who.int/api/{base}'.format(base=base)
 
-        # API fields of interest
-        self.fields = fields
-
-    def __request(self, url: str):
+    def request(self, params: dict) -> list:
 
         try:
-            response = requests.get(url=url, timeout=18)
+            response = requests.get(url=self.endpoint, params=params, timeout=18)
             response.raise_for_status()
         except requests.RequestException as err:
             raise Exception(err)
 
         if response.status_code != 200:
-            return None
+            return list()
         else:
             return response.json()
-
-    def exc(self, interests: list):
-        """
-        In progress
-
-        :return:
-        """
-
-        for interest in interests:
-
-            objects = self.__request(url=interest)
-            print(objects)
