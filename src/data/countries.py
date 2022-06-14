907b3c920c85e15647d2264a781797a903646c3f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-14 21:24:45 +0100

    Update src/data/countries.py

--- src/data/countries.py
+++ src/data/countries.py
@@ -1,6 +1,9 @@
+import os
+
 import pandas as pd
 
 import src.data.espen
+import src.functions.directories
 
 
 class Countries:
@@ -11,13 +14,26 @@ class Countries:
         :param key: API Key
         """
 
+        # API Key
         self.key = key
+
+        # The storage area of the countries file
+        self.storage = os.path.join(os.getcwd(), 'warehouse', 'gazetteer')
+        src.functions.directories.Directories().create(self.storage)
+
+        # The required fields, and the grouping fields for determining distinct
+        # data years per country
         self.fields = ['year', 'continent', 'region', 'who_region', 'admin0', 'admin0_id',
                        'iso2', 'iso3', 'admin_level']
         self.group = ['iso2', 'iso3', 'admin_level', 'admin0', 'admin0_id', 'region',
                       'who_region', 'continent']
 
     def __structure(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param data:
+        :return:
+        """
 
         frame = data.copy().loc[:, self.fields].drop_duplicates()
         countries = frame.groupby(self.group)[['year']].agg(lambda x: {', '.join(x.astype(str))})
@@ -26,9 +42,13 @@ class Countries:
         return countries
 
     def __write(self, data: pd.DataFrame):
+        """
 
-        data.to_csv(path_or_buf='')
+        :param data:
+        :return:
+        """
 
+        data.to_csv(path_or_buf=os.path.join(self.storage, 'countries.csv'), index=False, header=True, encoding='utf-8')
 
     def exc(self) -> pd.DataFrame:
         """
