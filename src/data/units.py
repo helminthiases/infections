b99acd05c57f564e3de5ff3687a891fe79adbe3c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-14 21:44:04 +0100

    Update src/data/units.py

--- src/data/units.py
+++ src/data/units.py
@@ -1,6 +1,10 @@
-import src.data.espen
+import logging
+import os
 
 import pandas as pd
+
+import src.data.espen
+import src.functions.directories
 
 
 class Units:
@@ -8,18 +12,45 @@ class Units:
     def __init__(self, key: str):
         """
 
-        :param key:
+        :param key: API Key
         """
 
+        # API Key
         self.key = key
-        self.fields = ['iso2', 'iso3', 'admin0_id', 'admin1', 'admin1_id', 'admin2', 'admin2_id', 'iu_name', 'iu_id', 'iu_code']
+
+        # The storage area of the countries file
+        self.storage = os.path.join(os.getcwd(), 'warehouse', 'gazetteer')
+        src.functions.directories.Directories().create(self.storage)
+
+        # The required fields
+        self.fields = ['iso2', 'iso3', 'admin0_id', 'admin1', 'admin1_id',
+                       'admin2', 'admin2_id', 'iu_name', 'iu_id', 'iu_code']
 
     def __structure(self, data: pd.DataFrame):
+        """
+
+        :param data:
+        :return:
+        """
 
         units = data.copy().loc[:, self.fields].drop_duplicates()
         conditions = units.loc[:, ['admin1_id', 'admin2_id', 'iu_id']].isna().any(axis='columns')
 
         return units.loc[~conditions, :]
+
+    def __write(self, data: pd.DataFrame):
+        """
+
+        :param data:
+        :return:
+        """
+
+        try:
+            data.to_csv(path_or_buf=os.path.join(self.storage, 'units.csv'),
+                        index=False, header=True, encoding='utf-8')
+            return True
+        except OSError as err:
+            raise Exception(err.strerror)
 
     def exc(self):
         """
@@ -29,8 +60,12 @@ class Units:
 
         objects = src.data.espen.ESPEN(base='cartographies').request(
             params={'api_key': self.key})
-
         frame = pd.DataFrame.from_records(objects)
         frame = self.__structure(data=frame)
 
-        return frame
+        if self.__write(data=frame):
+            logging.basicConfig(level=logging.INFO,
+                                format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                                datefmt='%Y-%m-%d %H:%M:%S')
+            logger = logging.getLogger(__name__)
+            logger.info('The countries gazetteer is available in {}'.format(self.storage))
