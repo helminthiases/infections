17f2b331851e0bbc9fa73bec627e18cea2552062
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-14 18:26:35 +0100

    Add src/data/points.py

---
+++ src/data/points.py
@@ -0,0 +1,35 @@
+import src.data.espen
+
+import pandas as pd
+
+
+class Points:
+
+    def __init__(self, key: str):
+        """
+
+        :param key:
+        """
+
+        self.key = key
+        self.fields = ['iso2', 'iso3', 'admin0_id', 'admin1_id', 'admin2_id', 'iu_id', 'iu_code']
+
+    @staticmethod
+    def __structure(data: pd.DataFrame):
+
+        print(data.head())
+        print(data.info())
+
+    def exc(self, level: str, iso2_strings: list):
+        """
+
+        :return:
+        """
+
+        interface = src.data.espen.ESPEN(base='data')
+
+        for iso2 in iso2_strings:
+
+            objects = interface.request(params={'api_key': self.key, 'disease': 'sth', 'level': level, 'iso2': iso2})
+            frame = pd.DataFrame.from_records(objects)
+            self.__structure(data=frame)
