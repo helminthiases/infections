c7de8cc395b89882e42b157c52a71d10e7c3a0af
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-14 21:10:34 +0100

    Add src/functions/directories.py

---
+++ src/functions/directories.py
@@ -0,0 +1,45 @@
+import os
+
+
+class Directories:
+
+    def __init__(self):
+        """
+
+        """
+
+    @staticmethod
+    def cleanup(path: str):
+        """
+        Clears a directory
+
+        :param path:
+        :return:
+        """
+
+        # Foremost, delete files
+        files_ = [os.remove(os.path.join(base, file))
+                  for base, _, files in os.walk(path) for file in files]
+
+        if any(files_):
+            raise Exception('Unable to delete all files within path {}'.format(path))
+
+        # ... then, directories
+        directories_ = [os.removedirs(os.path.join(base, directory))
+                        for base, directories, _ in os.walk(path, topdown=False)
+                        for directory in directories
+                        if os.path.exists(os.path.join(base, directory))]
+
+        if any(directories_):
+            raise Exception('Unable to delete all directories within path {}'.format(path))
+
+    @staticmethod
+    def create(path: str):
+        """
+
+        :param path:
+        :return:
+        """
+
+        if not os.path.exists(path):
+            os.makedirs(path)
