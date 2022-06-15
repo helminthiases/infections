7cb904a0d5ececcb2c84d80784e7559bd4f49197
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-15 16:39:19 +0100

    Update src/data/points.py

--- src/data/points.py
+++ src/data/points.py
@@ -1,38 +1,99 @@
-import src.data.espen
+import collections
+import os
 
+import dask
 import pandas as pd
+
+import src.data.espen
+import src.functions.directories
 
 
 class Points:
 
-    def __init__(self, key: str):
+    def __init__(self, parameter: collections.namedtuple('Parameter', ['api_key', 'disease', 'level']),
+                 fields: list):
         """
 
-        :param key:
+        :param parameter:
+        :param fields: The list of fields of interest
         """
 
-        self.key = key
-        self.fields = ['iso2', 'iso3', 'admin0_id', 'admin1_id', 'admin2_id', 'iu_id', 'iu_code']
+        self.parameter = parameter
+        self.fields = fields
 
-    @staticmethod
-    def __structure(data: pd.DataFrame):
+        # The storage area of the countries file
+        self.storage = os.path.join(os.getcwd(), 'warehouse', 'data', parameter.level)
+        src.functions.directories.Directories().create(self.storage)
 
-        points = data.copy()
-        points.rename(mapper=str.lower, axis='columns', inplace=True)
-        points.rename(columns={'admin1_code': 'admin1_id', 'admin2_code': 'admin2_id', 'siteid': 'site_id'}, inplace=True)
+        # An ESPEN interface instance
+        self.interface = src.data.espen.ESPEN(base='data')
 
-        print(points.info())
-
-    def exc(self, level: str, iso2_strings: list):
+    @dask.delayed
+    def __structure(self, data: pd.DataFrame):
         """
 
+        :param data:
         :return:
         """
 
-        interface = src.data.espen.ESPEN(base='data')
+        points = data.copy()
 
-        for iso2 in iso2_strings:
+        # Ensure that each field's name is lower-cased
+        points.rename(mapper=str.lower, axis='columns', inplace=True)
 
-            objects = interface.request(params={'api_key': self.key, 'disease': 'sth', 'level': level, 'iso2': iso2})
-            frame = pd.DataFrame.from_records(objects)
-            self.__structure(data=frame)
+        # Ensure field name consistency
+        points.rename(columns={'admin1_code': 'admin1_id', 'admin2_code': 'admin2_id', 'siteid': 'site_id'},
+                      inplace=True)
+
+        return points.loc[:, self.fields]
+
+    @dask.delayed
+    def __read(self, iso2: str):
+        """
+
+        :param iso2:
+        :return:
+        """
+
+        objects = self.interface.request(
+            params={'api_key': self.parameter.api_key, 'disease': self.parameter.disease,
+                    'level': self.parameter.level, 'iso2': iso2})
+        frame = pd.DataFrame.from_records(objects)
+
+        return frame
+
+    def __write(self, data: pd.DataFrame, name: str):
+        """
+
+        :param data:
+        :param name:
+        :return:
+        """
+
+        try:
+            data.to_csv(path_or_buf=os.path.join(self.storage, '{}.csv'.format(name)),
+                        index=False, header=True, encoding='utf-8')
+            return '{}: succeeded'.format(name)
+        except OSError as err:
+            raise Exception(err.strerror)
+
+    def exc(self, segments: list):
+        """
+
+        :param segments:
+        :return:
+        """
+
+        computations = []
+        for iso2 in segments:
+
+            frame = self.__read(iso2=iso2)
+            frame = self.__structure(data=frame)
+            message = self.__write(data=frame, name=iso2)
+
+            computations.append(message)
+
+        dask.visualize(computations, filename='data', format='pdf')
+        messages = dask.compute(computations, scheduler='processes')[0]
+
+        return messages
