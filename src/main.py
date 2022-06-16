6a29b88e5d6e4a5fbc0e835e4dacc06d6b450c98
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-16 14:57:07 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def main():
     logger.info('infections')
 
     # API Key
-    value = src.data.keys.Keys().exc(host='who')
+    value = src.helminth.keys.Keys().exc(host='who')
     logger.info(value)
 
     # Country codes
@@ -26,8 +26,8 @@ def main():
 
     # Get prevalence data per site of country
     Parameter = collections.namedtuple(typename='Parameter', field_names=['api_key', 'disease', 'level'])
-    points = src.data.points.Points(parameter=Parameter._make((value, 'sth', 'sitelevel')),
-                                    fields=fields.sites)
+    points = src.helminth.points.Points(parameter=Parameter._make((value, 'sth', 'sitelevel')),
+                                        fields=fields.sites)
     messages = points.exc(segments=segments)
     logger.info(messages)
 
@@ -46,10 +46,10 @@ if __name__ == '__main__':
 
     # libraries
     import config
-    import src.data.keys
-    import src.data.countries
-    import src.data.units
-    import src.data.points
+    import src.helminth.keys
+    import src.helminth.countries
+    import src.helminth.units
+    import src.helminth.points
 
     fields = config.Config().fields()
 
