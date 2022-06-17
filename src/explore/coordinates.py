b07b9c6d43bac04460f6b51df0676ea5d198b12a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-17 21:13:41 +0100

    Update src/explore/coordinates.py

--- src/explore/coordinates.py
+++ src/explore/coordinates.py
@@ -11,13 +11,17 @@ def main():
 
     items = glob.glob(pathname=os.path.join(path, '*.csv'))
 
-    for item in items:
+    for item in items[:1]:
 
         observations = pd.read_csv(filepath_or_buffer=item, header=0, encoding='utf-8')
         frame = gpd.GeoDataFrame(observations,
                                  geometry=gpd.points_from_xy(x=observations.longitude, y=observations.latitude))
-        frame.set_crs(4326, inplace=True)
+        frame.set_crs(crs='EPSG:4326', inplace=True)
         logger.info(frame.crs)
+        frame.to_crs(crs='EPSG:3857', inplace=True)
+        logger.info(frame.crs)
+        logger.info(frame.head().transpose())
+        logger.info(frame.info())
 
 
 if __name__ == '__main__':
