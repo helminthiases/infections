b9d92be42ab45df1321a2c0e34d7eb82347e718c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-17 20:57:33 +0100

    Update src/explore/coordinates.py

--- src/explore/coordinates.py
+++ src/explore/coordinates.py
@@ -1,20 +1,26 @@
-import os
-import logging
 import glob
+import logging
+import os
+
+import geopandas as gpd
+import pandas as pd
 
 
 def main():
-
     path = os.path.join(os.getcwd(), 'warehouse', 'data', 'sitelevel')
 
     items = glob.glob(pathname=os.path.join(path, '*.csv'))
 
     for item in items:
-        logger.info(item)
+
+        observations = pd.read_csv(filepath_or_buffer=item, header=0, encoding='utf-8')
+        frame = gpd.GeoDataFrame(observations,
+                                 geometry=gpd.points_from_xy(x=observations.longitude, y=observations.latitude))
+        frame.set_crs(4326, inplace=True)
+        logger.info(frame.crs)
 
 
 if __name__ == '__main__':
-
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d', datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(__name__)
