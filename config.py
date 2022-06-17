e4a1a169eca606ea0ce682a20ebd750430497098
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-17 21:47:30 +0100

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,6 @@ class Config:
                  'hk_examined', 'hk_positive', 'hk_perc_highinfection', 'hk_perc_moderateinfection',
                  'asc_examined', 'asc_positive', 'asc_perc_highinfection', 'asc_perc_moderateinfection',
                  'tt_examined', 'tt_positive', 'tt_perc_highinfection', 'tt_perc_moderateinfection',
-                 'sth_examined', 'sth_positive',
                  'quality', 'sn']
 
         units = ['iso3', 'iso2', 'admin1_id', 'admin2_id', 'iu_id', 'iu_code', 'year',
