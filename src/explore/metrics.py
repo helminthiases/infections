601f7e1d623d17201b2f19d0245c3952bb63ef32
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-17 16:36:09 +0100

    Add src/explore/metrics.py

---
+++ src/explore/metrics.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Metrics:
+
+    def __init__(self):
+        """
+
+        """
+
+        self.storage = os.path.join(os.getcwd(), 'warehouse')
+
+    def exc(self):
+        """
+
+        :return:
+        """
