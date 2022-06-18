38e50070023f402889cba71c5f678d2408f5e6f8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-18 18:20:56 +0100

    Update src/explore/identifiers.py

--- src/explore/identifiers.py
+++ src/explore/identifiers.py
@@ -23,11 +23,17 @@ class Identifiers:
         src.functions.directories.Directories().create(path=self.storage)
 
     @dask.delayed
-    def __read(self, item: str) -> pd.DataFrame:
+    def __inspect(self, item: str) -> pd.DataFrame:
+        """
+        Determines the number of observations/records, in a file, that have a site identification code
+
+        :param item: The file path of a data file
+        :return:
+        """
 
         observations = pd.read_csv(filepath_or_buffer=item, header=0, encoding='utf-8',
                                    usecols=['iso3', 'iso2', 'site_id'])
-        
+
         frame = observations[['iso3', 'iso2']].drop_duplicates()
         frame.loc[:, 'n_observations'] = observations.shape[0]
         frame.loc[:, 'n_site_identifiers'] = sum(observations['site_id'].notna())
@@ -46,11 +52,15 @@ class Identifiers:
             raise Exception(err.strerror)
 
     def exc(self) -> str:
+        """
+
+        :return:
+        """
 
         computations = []
         for item in self.items:
 
-            frame = self.__read(item=item)
+            frame = self.__inspect(item=item)
             computations.append(frame)
 
         dask.visualize(computations, filename='data', format='pdf')
