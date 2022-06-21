aa51f018db5e8d664267d25b760604ee7ac2ded1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-21 03:40:10 +0100

    Update src/source/prevalence.py

--- src/source/prevalence.py
+++ src/source/prevalence.py
@@ -18,8 +18,8 @@ class Prevalence:
     def individual(positive: pd.Series, examined: pd.Series):
         """
 
-        :param positive:
-        :param examined:
+        :param positive: positive cases amongst examinations
+        :param examined: the number of examinations conducted
         :return:
         """
 
@@ -28,10 +28,12 @@ class Prevalence:
     @staticmethod
     def geohelminth(ascariasis: pd.Series, trichuriasis: pd.Series, hookworm: pd.Series):
         """
+        Calculates the prevalence of any soil transmitted helminth infection via the prevalence values
+        of ascariasis, trichuriasis, and hookworm disease
 
-        :param ascariasis:
-        :param trichuriasis:
-        :param hookworm:
+        :param ascariasis: ascariasis prevalence
+        :param trichuriasis: trichuriasis prevalence
+        :param hookworm: hookworm prevalence
         :return:
         """
 
