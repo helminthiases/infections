d42ddb3a7e5404fe7c4f3ef026002f73691d51f8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-21 03:22:17 +0100

    Update src/source/espen.py

--- src/source/espen.py
+++ src/source/espen.py
@@ -1,14 +1,29 @@
+"""
+Module: espen
+"""
 import requests
 
 
 class ESPEN:
+    """
+    Queries, and extracts from, ESPEN repositories
+    """
 
     def __init__(self, base: str):
+        """
+
+        :param base: The ESPEN repository to read from, e.g., data, cartography, etc.
+        """
 
         # The API end point
         self.endpoint = 'https://admin.espen.afro.who.int/api/{base}'.format(base=base)
 
     def request(self, params: dict) -> list:
+        """
+
+        :param params: The ESPEN API parameters for querying an ESPEN repository
+        :return:
+        """
 
         try:
             response = requests.get(url=self.endpoint, params=params, timeout=33)
