40db55d6d3159b6937ed7435b399a34af4a28ad4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-23 12:13:28 +0100

    Update src/helminth/experiments.py

--- src/helminth/experiments.py
+++ src/helminth/experiments.py
@@ -4,55 +4,48 @@ Module: examinations
 import logging
 import os
 import sys
+import glob
+import dask
 
 import pandas as pd
 
 
-def main():
-    """
-    Reads ESPEN STH examinations data via the ESPEN API
+class Experiments:
 
-    :return:
-    """
+    def __init__(self):
+        """
 
-    # API Key
-    value = src.source.keys.Keys().exc(host='who')
-    logger.info(value)
+        """
 
-    # Country codes
-    try:
-        frame = pd.read_csv(filepath_or_buffer=os.path.join(os.getcwd(), 'warehouse', 'gazetteer', 'countries.csv'),
-                            header=0, usecols=['iso2'], dtype={'iso2': str}, encoding='utf8')
-    except OSError as err:
-        raise Exception(err.strerror) from err
-    codes = frame['iso2'].to_list()
-    codes = [code for code in codes if not pd.isnull(code)]
+        self.paths = glob.glob(os.path.join(os.getcwd(), 'data', 'ESPEN', 'experiments'))
 
-    # Get prevalence data per site of country
-    parameters = [{'api_key': value, 'disease': 'sth', 'level': 'sitelevel', 'iso2': iso2} for iso2 in codes]
-    points = src.source.points.Points(level='sitelevel', fields=fields.sites)
-    messages = points.exc(parameters=parameters)
-    logger.info(messages)
+    @dask.delayed
+    def __read(self, uri: str):
+        """
+        Reads ESPEN STH experiments data
+
+        :param uri: The local path + file name string
+        :return:
+        """
+
+        try:
+            frame = pd.read_json(path_or_buf=uri)
+        except OSError as err:
+            raise Exception(err.strerror) from err
+
+        return frame
+
+    
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        for path in self.paths:
+
+            self.__read(uri=path)
 
 
-if __name__ == '__main__':
 
-    root = os.getcwd()
-    sys.path.append(root)
-    sys.path.append(os.path.join(root, 'src'))
-
-    # Logging
-    logging.basicConfig(level=logging.INFO,
-                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger(__name__)
-
-    # libraries
-    import config
-    import src.source.keys
-    import src.helminth.countries
-    import src.source.points
-
-    fields = config.Config().fields()
-
-    main()
