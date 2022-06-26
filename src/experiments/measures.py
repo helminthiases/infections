2954aaf8031024d41bea190c2308d2a29ecb7515
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-26 19:38:49 +0100

    Update src/experiments/measures.py

--- src/experiments/measures.py
+++ src/experiments/measures.py
@@ -1,17 +1,56 @@
-import os
+import numpy as np
+import pandas as pd
 
 
-class Metrics:
+class Measures:
 
     def __init__(self):
         """
 
         """
 
-        self.storage = os.path.join(os.getcwd(), 'warehouse')
-
-    def exc(self):
+    @staticmethod
+    def __frequencies(ascariasis: pd.Series, trichuriasis: pd.Series, hookworm: pd.Series):
         """
+        the number of examinations per disease must be equivalent
 
+        :param ascariasis:
+        :param trichuriasis:
+        :param hookworm:
         :return:
         """
+
+        condition = (ascariasis == trichuriasis) == (ascariasis == hookworm)
+
+        return condition
+
+    @staticmethod
+    def __fractions(cases: pd.Series, examinations: pd.Series):
+        """
+        The number of cases must not exceed the number of examinations conducted
+
+        :param cases:
+        :param examinations:
+        :return:
+        """
+
+        condition = (cases <= examinations)
+
+        return condition
+
+    def exc(self, data: pd.DataFrame):
+        """
+
+        :param data:  An experiments data set
+        :return:
+        """
+
+        frequencies = self.__frequencies(ascariasis=data['asc_examined'],
+                                         trichuriasis=data['tt_examined'],
+                                         hookworm=data['hk_examined'])
+
+        fractions = np.empty(shape=[data.shape[0], 1])
+        for c, e in zip(['asc_positive', 'tt_positive', 'hk_positive'], ['asc_examined', 'tt_examined', 'hk_examined']):
+
+            fractions = np.concatenate((fractions,
+                                        self.__fractions(cases=data[c], examinations=data[e])))
