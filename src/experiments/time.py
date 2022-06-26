9003e9164b2a5ba534be9466050863c226380239
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-26 19:38:36 +0100

    Update src/experiments/time.py

--- src/experiments/time.py
+++ src/experiments/time.py
@@ -1,60 +1,38 @@
-import glob
-import logging
-import os
-import pathlib
-
-import geopandas as gpd
-import numpy as np
+"""
+Module: time
+"""
 import pandas as pd
 
 
-def main():
+class Time:
+    """
+    Drops the experiment records that do not have their time values
+    """
 
-    # The storage area of the site level data
-    path = os.path.join(os.getcwd(), 'warehouse', 'data', 'ESPEN', 'experiments')
+    def __init__(self):
+        """
 
-    # The list of files therein
-    items = glob.glob(pathname=os.path.join(path, '*.csv'))
+        """
 
-    # Exploring coordinate settings
-    for item in items[1:2]:
+    @staticmethod
+    def __year(data: pd.DataFrame):
+        """
 
-        logger.info(pathlib.Path(item).stem)
+        :param data: An experiments data frame
+        :return:
+        """
 
-        observations = pd.read_csv(filepath_or_buffer=item, header=0, encoding='utf-8')
-        observations = observations.copy().loc[observations['georeliability'] != 99, :]
+        frame = data.copy()[data['year'].notna(), :]
 
-        frame = gpd.GeoDataFrame(observations,
-                                 geometry=gpd.points_from_xy(x=observations.longitude, y=observations.latitude))
-        frame.set_crs(crs='EPSG:4326', inplace=True)
-        frame.to_crs(crs='EPSG:3857', inplace=True)
+        return frame
 
-        # distances
-        sample = frame.geometry.apply(lambda x: frame.distance(x)).values
-        sample = np.triu(m=sample, k=1)
+    def exc(self, data: pd.DataFrame):
+        """
 
-        # reference
-        reference = np.nan*np.ones_like(a=sample)
-        reference = np.tril(m=reference, k=0)
+        :param data: An experiments data frame
+        :return:
+        """
 
-        # distances within upper triangular area only
-        values: np.ndarray = reference + sample
-        index = values.shape[0] - 1
-        values[index, :] = values[:, index]
+        frame = self.__year(data=data)
 
-        L = np.all(np.isnan(values), axis=1)
-        logger.info(L)
-
-        beta = np.nanargmin(a=values, axis=1)
-        logger.info(beta)
-
-        sigma = np.nanmin(a=values, axis=1)
-        logger.info(sigma)
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO,
-                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d', datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger(__name__)
-
-    main()
+        return frame
