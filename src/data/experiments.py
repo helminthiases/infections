ba9d95d49fd4eb2c584a83328273edf928c87238
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-26 21:51:18 +0100

    Update src/data/experiments.py

--- src/data/experiments.py
+++ src/data/experiments.py
@@ -8,10 +8,12 @@ import pathlib
 import dask
 import pandas as pd
 
-import config
+import src.experiments.format
+import src.experiments.time
+import src.experiments.geographical
+import src.experiments.measures
 import src.functions.directories
 import src.functions.streams
-import src.experiments.format
 
 
 class Experiments:
@@ -24,17 +26,13 @@ class Experiments:
 
         """
 
-        self.paths = glob.glob(os.path.join(os.getcwd(), 'data', 'ESPEN', 'experiments', '*.json'))
-
-        # Data inspection instance
-        self.format = src.experiments.format.Format()
-
         # Reading and writing
         self.streams = src.functions.streams.Streams()
 
         # The storage area of the countries file
         self.storage = os.path.join(os.getcwd(), 'warehouse', 'data', 'ESPEN', 'experiments')
-        src.functions.directories.Directories().create(self.storage)
+        for directory in ['formatted', 'reduced', 'equivalent']:
+            src.functions.directories.Directories().create(os.path.join(self.storage, directory))
 
     @dask.delayed
     def __read(self, uri: str):
@@ -44,7 +42,7 @@ class Experiments:
         :param uri: The local path + file name string
         :return:
         """
-        
+
         return self.streams.read(uri=uri)
 
     @dask.delayed
@@ -59,11 +57,9 @@ class Experiments:
         if data.empty:
             return data
 
-        # Format
-        frame = self.format.exc(data=data)
+        frame = src.experiments.format.Format().exc(data=data)
 
-        # Write
-        self.streams.write(data=frame, path=os.path.join(self.storage, f'{name}.csv'))
+        self.streams.write(data=frame, path=os.path.join(self.storage, 'formatted', f'{name}.csv'))
 
         return frame
 
@@ -76,6 +72,14 @@ class Experiments:
         :return:
         """
 
+        frame = src.experiments.time.Time().exc(data=data)
+        frame = src.experiments.geographical.Geographical().exc(data=frame)
+        frame = src.experiments.measures.Measures().exc(data=frame)
+
+        self.streams.write(data=frame, path=os.path.join(self.storage, 'reduced', f'{name}.csv'))
+
+        return frame
+
     @dask.delayed
     def __equivalent(self, data: pd.DataFrame, name: str):
         """
@@ -84,21 +88,26 @@ class Experiments:
         :param name: 
         :return: 
         """
-        
+
+        return 'in progress'
+
     def exc(self):
         """
 
         :return:
         """
 
+        paths = glob.glob(os.path.join(os.getcwd(), 'data', 'ESPEN', 'experiments', '*.json'))
+
         computations = []
-        for path in self.paths:
+        for path in paths:
+
+            name = pathlib.Path(path).stem
+
             frame = self.__read(uri=path)
-            frame = self.__format(data=frame)
-
-
-
-            message = ''
+            frame = self.__format(data=frame, name=name)
+            frame = self.__reduce(data=frame, name=name)
+            message = self.__equivalent(data=frame, name=name)
 
             computations.append(message)
 
