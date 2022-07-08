d763b218f50000b15f0bdf242fae9dce15278db1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-08 21:30:17 +0100

    Update src/data/experiments.py

--- src/data/experiments.py
+++ src/data/experiments.py
@@ -83,6 +83,7 @@ class Experiments:
         frame = src.experiments.time.Time().exc(data=data)
         frame = src.experiments.geographical.Geographical().exc(data=frame)
         frame = src.experiments.intensity.Intensity().exc(data=frame)
+        frame = pd.DataFrame() if frame.shape[0] < 2 else frame
 
         self.streams.write(data=frame, path=os.path.join(self.storage, 'reduced', f'{name}.csv'))
 
